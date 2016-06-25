16065b7e67ef18375d775019b9ccf07c4b5ac012
Swolebrain <thecodingteacher@gmail.com>
2016-06-25 09:39:00 -0400

    db schema and connection added, failing from local to digi

--- server.js
+++ server.js
@@ -4,6 +4,38 @@ var express = require('express');
 var app = express();
 var jwt = require('express-jwt');
 var cors = require('cors');
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var db = require('./.configdb');
+
+var AppState = new Schema({
+  coreVals    : {
+                  SS: String,
+                  Acc: String,
+                  Asp: String,
+                  Com: String,
+                  Exx: String,
+                  Giv: String
+                },
+  evaluations : [
+                  {
+                    name        :   String,
+                    scoreCard   :   [
+                                      { name: String, score: Number, weight: Number}
+                                    ],
+                    coreVals    : {
+                            				SS: String,
+                            				Acc: String,
+                            				Asp: String,
+                            				Com: String,
+                            				Exx: String,
+                            				Giv: String
+                			             }
+                  }
+                ]
+});
+mongoose.connect('mongodb://' + db.user + ':' + db.pass + '@' + db.host + ':' + db.port + '/' + db.name);
+mongoose.model('AppState', AppState);
 
 var jwtCheck = jwt({
   secret: new Buffer('UUbQ3KTvY1B1xS-lD-fcSGpT3ZglxXsC20I6DHklKLCzHUD5ltmXsuF6muih5Uux', 'base64'),
@@ -18,8 +50,8 @@ var states = {};
 
 app.post('/api', (req,res) => {
   var user = req.query.user;
-  states[user] = req.query.state;
-  console.log("got a request - current state \n"+JSON.stringify(states));
+  //states[user] = req.query.state;
+  console.log("got a request - current state \n"+JSON.stringify(statesnode ));
   res.end("valid request from "+user+" state:\n");
 });
 
