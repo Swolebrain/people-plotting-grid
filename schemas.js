507b498cba8fdfb292a670b37044374d05f9e9af
Swolebrain <thecodingteacher@gmail.com>
2016-06-25 23:05:49 -0400

    refactored schemas to take out ids

--- schemas.js
+++ schemas.js
@@ -2,6 +2,21 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var db = require('./.configdb');
 
+var ScoreCardSchema = new Schema({ name: String, weight: String, score: String}, {_id: false});
+
+var SingleEvalSchema = new Schema({
+  name        :   String,
+  scorecard   :   [ ScoreCardSchema ],
+  coreVals    : {
+                  SS: String,
+                  Acc: String,
+                  Asp: String,
+                  Com: String,
+                  Exx: String,
+                  Giv: String
+                 }
+}, { _id: false });
+
 var AppStateSchema = new Schema({
   user: String,
   state: {
@@ -13,24 +28,9 @@ var AppStateSchema = new Schema({
                         Exx: String,
                         Giv: String
                       },
-        evals : [
-                        {
-                          name        :   String,
-                          scorecard   :   [
-                                            { name: String, weight: String, score: String}
-                                          ],
-                          coreVals    : {
-                                  				SS: String,
-                                  				Acc: String,
-                                  				Asp: String,
-                                  				Com: String,
-                                  				Exx: String,
-                                  				Giv: String
-                      			             }
-                        }
-                      ]
+        evals : [ SingleEvalSchema  ]
           }
-});
+}, {_id: false});
 mongoose.connect('mongodb://' + db.user + ':' + db.pass + '@' + db.host + ':' + db.port + '/' + db.name);
 module.exports = {
   AppState : mongoose.model('AppState', AppStateSchema)
